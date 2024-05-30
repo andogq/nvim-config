bcfafc2a0a71634731b94e3eb152fbfedc923a13
Tom Anderson <tom@ando.gq>
2024-05-30 14:04:16 +1000

    maybe broken changes

--- init.lua
+++ init.lua
@@ -59,6 +59,114 @@ require('lazy').setup({
       -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',
     },
+    ---@class PluginLspOpts
+    opts = function()
+      return {
+        {
+          -- options for vim.diagnostic.config()
+          ---@type vim.diagnostic.Opts
+          diagnostics = {
+            underline = true,
+            update_in_insert = false,
+            virtual_text = {
+              spacing = 4,
+              source = "if_many",
+              prefix = "●",
+              -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
+              -- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
+              -- prefix = "icons",
+            },
+            severity_sort = true,
+            signs = {
+              [vim.diagnostic.severity.ERROR] = "󰅚 ",
+              [vim.diagnostic.severity.WARN] = "󰀪 ",
+              [vim.diagnostic.severity.HINT] = "󰌶 ",
+              [vim.diagnostic.severity.INFO] = " ",
+            },
+          },
+        },
+        -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
+        -- Be aware that you also will need to properly configure your LSP server to
+        -- provide the inlay hints.
+        inlay_hints = {
+          enabled = true,
+          exclude = {}, -- filetypes for which you don't want to enable inlay hints
+        },
+        -- Enable this to enable the builtin LSP code lenses on Neovim >= 0.10.0
+        -- Be aware that you also will need to properly configure your LSP server to
+        -- provide the code lenses.
+        codelens = {
+          enabled = false,
+        },
+        -- Enable lsp cursor word highlighting
+        document_highlight = {
+          enabled = true,
+        },
+        -- add any global capabilities here
+        capabilities = {
+          workspace = {
+            fileOperations = {
+              didRename = true,
+              willRename = true,
+            },
+          },
+        },
+        -- options for vim.lsp.buf.format
+        -- `bufnr` and `filter` is handled by the LazyVim formatter,
+        -- but can be also overridden when specified
+        format = {
+          formatting_options = nil,
+          timeout_ms = nil,
+        },
+        -- LSP Server Settings
+        ---@type lspconfig.options
+        servers = {
+          lua_ls = {
+            -- mason = false, -- set to false if you don't want this server to be installed with mason
+            -- Use this to add any additional keymaps
+            -- for specific lsp servers
+            ---@type LazyKeysSpec[]
+            -- keys = {},
+            settings = {
+              Lua = {
+                workspace = {
+                  checkThirdParty = false,
+                },
+                codeLens = {
+                  enable = true,
+                },
+                completion = {
+                  callSnippet = "Replace",
+                },
+                doc = {
+                  privateName = { "^_" },
+                },
+                hint = {
+                  enable = true,
+                  setType = false,
+                  paramType = true,
+                  paramName = "Disable",
+                  semicolon = "Disable",
+                  arrayIndex = "Disable",
+                },
+              },
+            },
+          },
+        },
+        -- you can do any additional lsp server setup here
+        -- return true if you don't want this server to be setup with lspconfig
+        ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
+        setup = {
+          -- example to setup with typescript.nvim
+          -- tsserver = function(_, opts)
+          --   require("typescript").setup({ server = opts })
+          --   return true
+          -- end,
+          -- Specify * to use this function as a fallback for any server
+          -- ["*"] = function(server, opts) end,
+        },
+      }
+    end,
     init = function()
       local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
       for type, icon in pairs(signs) do
@@ -401,7 +509,7 @@ require('which-key').register {
 -- mason-lspconfig requires that these setup functions are called in this order
 -- before setting up the servers.
 require('mason').setup()
-require('mason-lspconfig').setup()
+-- require('mason-lspconfig').setup()
 
 -- Enable the following language servers
 --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -415,7 +523,6 @@ local servers = {
   -- clangd = {},
   -- gopls = {},
   -- pyright = {},
-  -- rust_analyzer = {},
   -- tsserver = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
 
@@ -426,35 +533,35 @@ local servers = {
     },
   },
 
-  rust_analyzer = {
-    keys = {
-      { "K",          "<cmd>RustHoverActions<cr>", desc = "Hover Actions (Rust)" },
-      { "<leader>cR", "<cmd>RustCodeAction<cr>",   desc = "Code Action (Rust)" },
-      { "<leader>dr", "<cmd>RustDebuggables<cr>",  desc = "Run Debuggables (Rust)" },
-    },
-    settings = {
-      ["rust-analyzer"] = {
-        cargo = {
-          allFeatures = true,
-          loadOutDirsFromCheck = true,
-          runBuildScripts = true,
-        },
-        checkOnSave = {
-          allFeatures = true,
-          command = "clippy",
-          extraArgs = { "--no-deps" },
-        },
-        procMacro = {
-          enable = true,
-          ignored = {
-            ["async-trait"] = { "async_trait" },
-            ["napi-derive"] = { "napi" },
-            ["async-recursion"] = { "async_recursion" },
-          },
-        },
-      },
-    },
-  },
+  -- rust_analyzer = {
+  --   keys = {
+  --     { "K",          "<cmd>RustHoverActions<cr>", desc = "Hover Actions (Rust)" },
+  --     { "<leader>cR", "<cmd>RustCodeAction<cr>",   desc = "Code Action (Rust)" },
+  --     { "<leader>dr", "<cmd>RustDebuggables<cr>",  desc = "Run Debuggables (Rust)" },
+  --   },
+  --   settings = {
+  --     ["rust-analyzer"] = {
+  --       cargo = {
+  --         allFeatures = true,
+  --         loadOutDirsFromCheck = true,
+  --         runBuildScripts = true,
+  --       },
+  --       checkOnSave = {
+  --         allFeatures = true,
+  --         command = "clippy",
+  --         extraArgs = { "--no-deps" },
+  --       },
+  --       procMacro = {
+  --         enable = true,
+  --         ignored = {
+  --           ["async-trait"] = { "async_trait" },
+  --           ["napi-derive"] = { "napi" },
+  --           ["async-recursion"] = { "async_recursion" },
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
 
   clangd = {
     filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
@@ -491,9 +598,7 @@ mason_lspconfig.setup_handlers {
         vim.api.nvim_create_autocmd("BufWritePost", {
           pattern = { "*.js", "*.ts" },
           callback = function(ctx)
-            if client.name == "svelte" then
-              client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.file })
-            end
+            client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
           end,
         });
 
--- lua/custom/plugins/rust.lua
+++ lua/custom/plugins/rust.lua
@@ -61,44 +61,90 @@ return {
     --     }
     -- },
     --
+    -- {
+    --     "simrat39/rust-tools.nvim",
+    --     lazy = true,
+    --     opts = function()
+    --         local ok, mason_registry = pcall(require, "mason-registry")
+    --         local adapter ---@type any
+    --         if ok then
+    --             -- rust tools configuration for debugging support
+    --             local codelldb = mason_registry.get_package("codelldb")
+    --             local extension_path = codelldb:get_install_path() .. "/extension/"
+    --             local codelldb_path = extension_path .. "adapter/codelldb"
+    --             local liblldb_path = ""
+    --             if vim.loop.os_uname().sysname:find("Windows") then
+    --                 liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
+    --             elseif vim.fn.has("mac") == 1 then
+    --                 liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
+    --             else
+    --                 liblldb_path = extension_path .. "lldb/lib/liblldb.so"
+    --             end
+    --             adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
+    --         end
+    --         return {
+    --             dap = {
+    --                 adapter = adapter,
+    --             },
+    --             tools = {
+    --                 on_initialized = function()
+    --                     vim.cmd([[
+    --               augroup RustLSP
+    --                 autocmd CursorHold                      *.rs silent! lua vim.lsp.buf.document_highlight()
+    --                 autocmd CursorMoved,InsertEnter         *.rs silent! lua vim.lsp.buf.clear_references()
+    --                 autocmd BufEnter,CursorHold,InsertLeave *.rs silent! lua vim.lsp.codelens.refresh()
+    --               augroup END
+    --             ]])
+    --                 end,
+    --             },
+    --         }
+    --     end,
+    --     config = function() end,
+    -- },
     {
-        "simrat39/rust-tools.nvim",
-        lazy = true,
-        opts = function()
-            local ok, mason_registry = pcall(require, "mason-registry")
-            local adapter ---@type any
-            if ok then
-                -- rust tools configuration for debugging support
-                local codelldb = mason_registry.get_package("codelldb")
-                local extension_path = codelldb:get_install_path() .. "/extension/"
-                local codelldb_path = extension_path .. "adapter/codelldb"
-                local liblldb_path = ""
-                if vim.loop.os_uname().sysname:find("Windows") then
-                    liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
-                elseif vim.fn.has("mac") == 1 then
-                    liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
-                else
-                    liblldb_path = extension_path .. "lldb/lib/liblldb.so"
-                end
-                adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
-            end
-            return {
-                dap = {
-                    adapter = adapter,
-                },
-                tools = {
-                    on_initialized = function()
-                        vim.cmd([[
-                  augroup RustLSP
-                    autocmd CursorHold                      *.rs silent! lua vim.lsp.buf.document_highlight()
-                    autocmd CursorMoved,InsertEnter         *.rs silent! lua vim.lsp.buf.clear_references()
-                    autocmd BufEnter,CursorHold,InsertLeave *.rs silent! lua vim.lsp.codelens.refresh()
-                  augroup END
-                ]])
-                    end,
-                },
+        'mrcjkb/rustaceanvim',
+        version = '^4',
+        lazy = false,
+        ft = { "rust" },
+        opts = {
+            server = {
+                on_attach = function(client, bufnr)
+                    local wk = require("which-key")
+                    wk.register({
+                        ["<leader>cR"] = { function() vim.cmd.RustLsp("codeAction") end, "Code Action" },
+                        ["<leader>dr"] = { function() vim.cmd.RustLsp("debuggables") end, "Rust debuggables" },
+                    }, { mode = "n", buffer = bufnr })
+                end,
+                default_settings = {
+                    -- rust-analyzer language server configuration
+                    ["rust-analyzer"] = {
+                        cargo = {
+                            allFeatures = true,
+                            loadOutDirsFromCheck = true,
+                            runBuildScripts = true,
+                        },
+                        -- Add clippy lints for Rust.
+                        checkOnSave = {
+                            allFeatures = true,
+                            command = "clippy",
+                            extraArgs = { "--no-deps" },
+                        },
+                        procMacro = {
+                            enable = true,
+                            ignored = {
+                                ["async-trait"] = { "async_trait" },
+                                ["napi-derive"] = { "napi" },
+                                ["async-recursion"] = { "async_recursion" },
+                            },
+                        },
+                    }
+                }
             }
-        end,
-        config = function() end,
-    },
+        },
+        config = function(_, opts)
+            vim.g.rustaceanvim = vim.tbl_deep_extend("force",
+                {},
+                opts or {})
+        end
+    }
 }
