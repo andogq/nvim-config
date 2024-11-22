084d54f08c09866e37abd29d663bdad72c7ad357
Tom Anderson <tom@ando.gq>
2024-11-22 19:51:52 +1100

    always run custom `on_attach` for LSP buffers

--- init.lua
+++ init.lua
@@ -63,12 +63,6 @@ local servers = {
 }
 
 local on_attach = function(_, bufnr)
-  -- NOTE: Remember that lua is a real programming language, and as such it is possible
-  -- to define small helper and utility functions so you don't have to repeat yourself
-  -- many times.
-  --
-  -- In this case, we create a function that lets us more easily define mappings specific
-  -- for LSP related items. It sets the mode, buffer and description for us each time.
   local nmap = function(keys, func, desc)
     if desc then
       desc = 'LSP: ' .. desc
@@ -198,6 +192,8 @@ require('lazy').setup({
               -- Call the default one
               on_attach(client, bufnr)
             end
+          else
+            server.on_attach = on_attach
           end
 
           require('lspconfig')[server_name].setup(server)
