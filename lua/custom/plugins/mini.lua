bd79b71aa90ed27790f1031ac3fea8d211e881f3
Tom Anderson <tom@ando.sh>
2025-09-03 15:34:09 +1000

    chore(mini.nvim): update to `nvim-mini` org

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -1,5 +1,5 @@
 return { -- Collection of various small independent plugins/modules
-  'echasnovski/mini.nvim',
+  'nvim-mini/mini.nvim',
   config = function()
     -- Better Around/Inside textobjects
     require('mini.ai').setup { n_lines = 500 }
