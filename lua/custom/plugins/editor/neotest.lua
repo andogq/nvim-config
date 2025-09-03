b23d68e88758f84f99befecd7df110afcb2c4b77
Tom Anderson <tom@ando.sh>
2025-09-03 15:10:02 +1000

    setup neotest

---
+++ lua/custom/plugins/editor/neotest.lua
@@ -0,0 +1,68 @@
+return {
+  'nvim-neotest/neotest',
+  version = '^5',
+  dependencies = {
+    'nvim-neotest/nvim-nio',
+    'nvim-lua/plenary.nvim',
+    'antoinemadec/FixCursorHold.nvim',
+    'nvim-treesitter/nvim-treesitter',
+  },
+  opts = function()
+    return {
+      adapters = {
+        require 'rustaceanvim.neotest',
+      },
+    }
+  end,
+  keys = {
+    {
+      '<leader>tw',
+      function()
+        require('neotest').watch.toggle(vim.fn.getcwd())
+      end,
+      desc = '[W]atch tests',
+    },
+    {
+      '<leader>tr',
+      function()
+        require('neotest').run.run(vim.fn.getcwd())
+      end,
+      desc = '[R]un tests',
+    },
+    {
+      '<leader>tfr',
+      function()
+        require('neotest').run.run(vim.fn.expand '%')
+      end,
+      desc = '[R]un file tests',
+    },
+    {
+      '<leader>tfw',
+      function()
+        require('neotest').watch.toggle(vim.fn.expand '%')
+      end,
+      desc = '[W]atch file tests',
+    },
+    {
+      '<leader>tcr',
+      function()
+        require('neotest').run.run()
+      end,
+      desc = '[R]un closest test',
+    },
+    {
+      '<leader>tcw',
+      function()
+        require('neotest').watch.toggle()
+      end,
+      desc = '[W]atch closest test',
+    },
+    {
+      '<leader>ts',
+      function()
+        require('neotest').summary.toggle()
+      end,
+      desc = 'Toggle test [s]ummary window',
+    },
+  },
+}
