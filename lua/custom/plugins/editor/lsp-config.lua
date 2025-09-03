f178698244e17703a986b9a1ff6a37f85138f6da
Tom Anderson <tom@ando.sh>
2025-09-03 15:32:44 +1000

    fix(diagnostics): float diagnostics on jump, and always show underline

--- lua/custom/plugins/editor/lsp-config.lua
+++ lua/custom/plugins/editor/lsp-config.lua
@@ -84,7 +84,6 @@ return {
     vim.diagnostic.config {
       severity_sort = true,
       float = { border = 'rounded', source = 'if_many' },
-      underline = { severity = vim.diagnostic.severity.ERROR },
       signs = vim.g.have_nerd_font and {
         text = {
           [vim.diagnostic.severity.ERROR] = '󰅚 ',
@@ -93,6 +92,7 @@ return {
           [vim.diagnostic.severity.HINT] = '󰌶 ',
         },
       } or {},
+      jump = { float = true },
       virtual_text = {
         source = 'if_many',
         spacing = 2,
