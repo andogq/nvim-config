d52be62e76c3de6fe93af0305793a78ecc5a0dd7
Tom Anderson <tom@ando.sh>
2025-11-27 14:02:35 +1100

    fix(nvim-lint): use `eslint_d` for TS files

--- lua/custom/plugins/editor/nvim-lint.lua
+++ lua/custom/plugins/editor/nvim-lint.lua
@@ -10,6 +10,7 @@ return {
       javascriptreact = { 'eslint_d' },
       markdown = { 'markdownlint' },
       python = { 'djlint' },
+      typescript = { 'eslint_d' },
       typescriptreact = { 'eslint_d' },
     }
 
